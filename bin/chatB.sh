e6cfc5aaac25b5f24d3123897e56bcac43c6241e
Bela Ban <belaban@yahoo.com>
2018-03-20 17:51:55 +0100

    Added Chat demo

---
+++ bin/chatB.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+RELAY_DIR="$DIR/../jgroups-4.x"
+
+cd $RELAY_DIR && mvn -o exec:java -Dexec.mainClass=org.jgroups.demos.Chat \
+ -Dexec.args="-props config.xml -name B"
