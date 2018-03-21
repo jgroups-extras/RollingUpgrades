032ba5d630ab01d85edea7c5aecbd3821a9a41a2
Bela Ban <belaban@yahoo.com>
2018-03-21 11:12:12 +0100

    ns

--- bin/relay-server.sh
+++ bin/relay-server.sh
@@ -3,4 +3,4 @@
 DIR=`dirname $0`
 RELAY_DIR="$DIR/../relay-server"
 
-cd $RELAY_DIR && mvn -o exec:java -Dexec.mainClass=org.jgroups.relay_server.RelayServer
+cd $RELAY_DIR && mvn exec:java -Dexec.mainClass=org.jgroups.relay_server.RelayServer
