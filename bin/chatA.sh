6a11d4cc6d03f62fd09890bd6e393cbd6208e716
Bela Ban <belaban@yahoo.com>
2018-03-26 11:07:36 +0200

    ns

--- bin/chatA.sh
+++ bin/chatA.sh
@@ -3,5 +3,5 @@
 DIR=`dirname $0`
 RELAY_DIR="$DIR/../jgroups-4.x"
 
-cd $RELAY_DIR && mvn -o exec:java -Dexec.mainClass=org.jgroups.demos.Chat \
+cd $RELAY_DIR && mvn exec:java -Dexec.mainClass=org.jgroups.demos.Chat \
  -Dexec.args="-props config.xml -name A"
