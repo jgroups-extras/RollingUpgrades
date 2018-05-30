c4eb0e79fd61ac1d95c689342ba06831f8271fe9
Bela Ban <belaban@yahoo.com>
2018-05-30 07:22:07 +0200

    ns

--- bin/drawA.sh
+++ bin/drawA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=`dirname $0`
-RELAY_DIR="$DIR/../jgroups-4.x"
+RELAY_DIR="$DIR/../jgroups-4"
 SYSPROPS="-Dcom.sun.management.jmxremote -Djava.net.preferIPv4Stack=true -Dlog4j.configurationFile=/home/bela/log4j2.xml"
 
 cd $RELAY_DIR && mvn $SYSPROPS exec:java -Dexec.mainClass=org.jgroups.demos.Draw \
