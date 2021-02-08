394f1dc06c1123e746a1aca2f97420e6d191fd65
Bela Ban <belaban@mailbox.org>
2021-02-08 10:58:25 +0100

    ns

--- jgroups-5/src/main/java/org/jgroups/demos/MessageDispatcherTest.java
+++ jgroups-5/src/main/java/org/jgroups/demos/MessageDispatcherTest.java
@@ -82,7 +82,7 @@ public class MessageDispatcherTest implements RequestHandler {
             Message msg=new BytesMessage(null, b);
             RspList<DemoResponse> rsps=disp.castMessage(null, msg, RequestOptions.SYNC());
             System.out.printf("rsps:\n%s\n", rsps.entrySet().stream()
-              .filter(e -> e.getKey() != null && e.getValue() != null)
+              .filter(e -> e.getKey() != null && e.getValue() != null && e.getValue().getValue() != null)
               .map(e -> String.format("%s: %s", e.getKey(), e.getValue().getValue().getCount()))
               .collect(Collectors.joining("\n")));
         }
