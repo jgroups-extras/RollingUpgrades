e1fb54dc1ac778c40845231956db65f103272340
Bela Ban <belaban@mailbox.org>
2021-03-03 11:56:39 +0100

    Added GrpcClient which handles connection to the gRPC server, plus sending and receiving of messages

---
+++ common/src/main/java/org/jgroups/common/GrpcClient.java
@@ -0,0 +1,165 @@
+package org.jgroups.common;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import io.netty.handler.ssl.SslContext;
+import org.jgroups.upgrade_server.*;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+/**
+ * Class which interacts with a gRPC server, e.g. sending and receiving messages, retry logic etc. The state transitions
+ * are start - connect - disconnect (possibly multiple times) - stop
+ * @author Bela Ban
+ * @since  1.1.1
+ */
+public class GrpcClient implements StreamObserver<Response> {
+    protected String                                server_address="localhost";
+    protected int                                   server_port=50051;
+    protected String                                server_cert;
+    protected ManagedChannel                        channel;
+    protected UpgradeServiceGrpc.UpgradeServiceStub asyncStub;
+    protected StreamObserver<Request>               send_stream;
+    protected final Lock                            send_stream_lock=new ReentrantLock();
+    protected final Set<Consumer<View>>             view_handlers=new HashSet<>();
+    protected final Set<Consumer<Message>>          message_handlers=new HashSet<>();
+    protected static final Logger                   log=Logger.getLogger(GrpcClient.class.getSimpleName());
+
+    public String     getServerAddress()                        {return server_address;}
+    public GrpcClient setServerAddress(String a)                {server_address=a; return this;}
+    public int        getServerPort()                           {return server_port;}
+    public GrpcClient setServerPort(int p)                      {server_port=p; return this;}
+    public String     getServerCert()                           {return server_cert;}
+    public GrpcClient setServerCert(String c)                   {server_cert=c; return this;}
+    public GrpcClient addViewHandler(Consumer<View> h)          {view_handlers.add(h); return this;}
+    public GrpcClient removeViewHandler(Consumer<View> h)       {view_handlers.remove(h); return this;}
+    public GrpcClient addMessageHandler(Consumer<Message> h)    {message_handlers.add(h); return this;}
+    public GrpcClient removeMessageHandler(Consumer<Message> h) {message_handlers.remove(h); return this;}
+
+
+    public GrpcClient start() throws Exception {
+        InputStream server_cert_stream=null;
+        SslContext  ctx=null;
+
+        if(server_cert != null && !server_cert.trim().isEmpty()) {
+            if((server_cert_stream=Utils.getFile(server_cert)) == null)
+                throw new FileNotFoundException(String.format("server certificate (%s) not found", server_cert));
+            ctx=GrpcSslContexts.forClient().trustManager(server_cert_stream).build();
+        }
+        NettyChannelBuilder cb=NettyChannelBuilder.forAddress(server_address, server_port);
+        if(server_cert_stream == null)
+            channel=cb.usePlaintext().build();
+        else
+            channel=cb.sslContext(ctx).build();
+        asyncStub=UpgradeServiceGrpc.newStub(channel);
+        return this;
+    }
+
+    public GrpcClient stop() {
+        if(channel != null) {
+            channel.shutdown();
+            try {
+                channel.awaitTermination(30, TimeUnit.SECONDS);
+            }
+            catch(InterruptedException e) {
+            }
+        }
+        return this;
+    }
+
+    public synchronized GrpcClient connect(String cluster, Address local_addr) {
+        send_stream=asyncStub.connect(this);
+        JoinRequest join_req=JoinRequest.newBuilder().setAddress(local_addr).setClusterName(cluster).build();
+        Request req=Request.newBuilder().setJoinReq(join_req).build();
+        send_stream.onNext(req);
+        return this;
+    }
+
+    public synchronized GrpcClient disconnect(String cluster, Address local_addr) {
+        if(send_stream != null) {
+            if(local_addr != null && cluster != null) {
+                LeaveRequest leave_req=LeaveRequest.newBuilder().setClusterName(cluster).setLeaver(local_addr).build();
+                Request request=Request.newBuilder().setLeaveReq(leave_req).build();
+                send_stream.onNext(request);
+            }
+            send_stream.onCompleted();
+        }
+        return this;
+    }
+
+    public GrpcClient send(Request req) {
+        send_stream_lock.lock();
+        try {
+            // Per javadoc, StreamObserver is not thread-safe and calls onNext() must be handled by the application
+            send_stream.onNext(req);
+            return this;
+        }
+        finally {
+            send_stream_lock.unlock();
+        }
+    }
+
+    public void onNext(Response rsp) {
+        if(rsp.hasMessage()) {
+            handleMessage(rsp.getMessage());
+            return;
+        }
+        if(rsp.hasView()) {
+            handleView(rsp.getView());
+            return;
+        }
+        throw new IllegalStateException(String.format("response is illegal: %s", rsp));
+    }
+
+    public void onError(Throwable t) {
+        log.warning(String.format("exception from server: %s", t));
+    }
+
+    public void onCompleted() {
+        log.info("server is done");
+    }
+
+    protected void handleMessage(Message msg) {
+        for(Consumer<Message> c: message_handlers)
+            c.accept(msg);
+    }
+
+    protected void handleView(View view) {
+        for(Consumer<View> c: view_handlers)
+            c.accept(view);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        GrpcClient client=new GrpcClient()
+          .addMessageHandler(m -> System.out.printf("-- msg from %s: %s\n",
+                                                    m.getSender().getName(), new String(m.getPayload().toByteArray())))
+          .addViewHandler(v -> System.out.printf("-- view: %s\n", v))
+          .start();
+
+
+        UUID uuid=UUID.newBuilder().setLeastSig(1).setMostSig(2).build();
+        Address a=Address.newBuilder().setUuid(uuid).setName("A").build();
+        client.connect("rpcs", a);
+
+        byte[] buf="hello world".getBytes();
+        Message msg=Message.newBuilder()
+          .setClusterName("rpcs")
+          .setSender(Address.newBuilder().setName("A").setUuid(uuid).build())
+          .setPayload(ByteString.copyFrom(buf)).build();
+        client.send(Request.newBuilder().setMessage(msg).build());
+        client.disconnect("rpcs", a);
+
+    }
+}
